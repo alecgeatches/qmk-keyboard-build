a5f53fa1154a54411adb09f298550c459a1986ca
Alec Geatches <alecgeatches@outlook.com>
2019-03-14 18:59:39 +0800

    Switch * and % symbol layer keys, add simple white sine wave animation for plasma animation

--- src/animation/plasma.c
+++ src/animation/plasma.c
@@ -1,32 +1,63 @@
 #include "ergodox_ez.h"
 
 #define ALECG_PLASMA_TICKS_PER_FRAME 2
+#define ALECG_PLASMA_DISTANCE_DROPOFF 100 // Out of 255
+#define ALECG_PLASMA_MINIMUM_INTENSITY 50 // Out of 255
 
 uint8_t fastled_sin8(uint8_t theta);
+uint8_t fastled_sqrt16(uint16_t x);
 
 void alecg_animate_plasma(uint32_t tick) {
+    static uint8_t points[ALECG_RGB_COLUMNS] = {0};
     static uint8_t offset = 0;
 
     if((tick % ALECG_PLASMA_TICKS_PER_FRAME) == 0) {
         offset += 1;
     }
 
+    for(uint8_t i = 0; i < ALECG_RGB_COLUMNS; i++) {
+        points[i] = fastled_sin8(i * 20 + offset);
+    }
+
     // Draw board
-    uint8_t led_i, color;
-    // RGB rgb;
+    uint8_t led_i, intensity;
+    HSV hsv = {.h = 0, .s = 0, .v = 255};
+    RGB rgb;
 
     for(uint8_t row = 0; row < ALECG_RGB_ROWS; row++) {
         for(uint8_t column = 0; column < ALECG_RGB_COLUMNS; column++) {
-            color = fastled_sin8((column + row) * 50 - offset);
+            // Diagonal:
+            // color = fastled_sin8((column + row) * 50 - offset);
+
+            // Circular:
+            // uint8_t distance_row_2 = ((float)row - (ALECG_RGB_ROWS / 2.0f)) * ((float)row - (ALECG_RGB_ROWS / 2.0f));
+            // uint8_t distance_column_2 = ((float)column - (ALECG_RGB_COLUMNS / 2.0f)) * ((float)column - (ALECG_RGB_COLUMNS / 2.0f));
+            // color = fastled_sin8(fastled_sqrt16(distance_row_2 + distance_column_2) * 50 - offset);;
+
+            // Red fire hue between 0 and 43
+            // Blue fire hue between 160 and 173
+            uint8_t row_value = 255.0f * ((float)row/(ALECG_RGB_ROWS - 1));
+            uint8_t distance_from_point = abs((int16_t)row_value - (int16_t)points[column]);
+
+            if(distance_from_point > ALECG_PLASMA_DISTANCE_DROPOFF) {
+                intensity = 0;
+            } else {
+                intensity = 255.0f * (1.0f - (float)distance_from_point / ALECG_PLASMA_DISTANCE_DROPOFF);
+            }
+
+            intensity = MAX(intensity, ALECG_PLASMA_MINIMUM_INTENSITY);
+
+            hsv.v = intensity;
 
             led_i = alecg_get_led_by_position(row, column);
-            rgb_matrix_set_color(led_i, color, color, color);
+            rgb = hsv_to_rgb(hsv);
+            rgb_matrix_set_color(led_i, rgb.r, rgb.g, rgb.b);
         }
     }
 }
 
 // FastLED 3.1's sin8_C function
-// http://fastled.io/docs/3.1/group___trig.html#ga46933a9b42921c6605588268d11f7c1e
+// http://fastled.io/docs/3.1/group___trig.html
 const uint8_t b_m16_interleave[] = {0, 49, 49, 41, 90, 27, 117, 10};
 uint8_t fastled_sin8(uint8_t theta) {
     uint8_t offset = theta;
@@ -55,3 +86,6 @@ uint8_t fastled_sin8(uint8_t theta) {
 
     return y;
 }
+
+// FastLED 3.1's sqrt16 function
+// http://fastled.io/docs/3.1/group___math.html
