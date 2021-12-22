51c7befdca6c48892261a4fa1951c08cf7cad792
Alec Geatches <alecgeatches@outlook.com>
2021-12-22 10:50:29 +0800

    Add previous changes before switching to ZSA compiled version

--- src/rules.mk
+++ src/rules.mk
@@ -4,6 +4,7 @@ COMMAND_ENABLE = no
 SRC = matrix.c
 RGBLIGHT_ENABLE = no
 RGB_MATRIX_ENABLE = yes
+UNICODE_ENABLE = yes
 
 MOUSEKEY_ENABLE = no # Mouse keys(+4700)
 COMMAND_ENABLE = no # Commands for debug and configuration
